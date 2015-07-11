0db53bed1b9373a1d9a231400593cde73479910e
JNine <jonathan.kurvits@gmail.com>
2015-07-11 19:02:47 -0400

    added delete

--- client/js/noteItem.js
+++ client/js/noteItem.js
@@ -2,4 +2,10 @@ Template.noteItem.helpers({
   formattedDate:function(createdAt){
     return createdAt.toLocaleDateString()
   }
-})
+});
+
+Template.noteItem.events({
+  'click .delete':function(){
+    Notes.remove(this._id)
+  }
+});
