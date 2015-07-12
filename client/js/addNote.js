622da8229b2f261b359595c4c52cd57b90061101
Ben <ishmandoo@gmail.com>
2015-07-12 09:24:07 -0400

    changed default content

--- client/js/addNote.js
+++ client/js/addNote.js
@@ -1,8 +1,8 @@
 Template.addNote.events({
   'click button':function(){
     Notes.insert(
-      {name:'untitled note',
-      content:'new note',
+      {name:'untitled',
+      content:'',
       createdAt:new Date()},
       function (err, id) {
         Router.go("/notes/"+id)
