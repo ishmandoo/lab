1e468c25605adc60f9ad03ac89bfc9d0441680b6
Ben <ishmandoo@gmail.com>
2015-07-11 22:03:38 -0400

    hacked our way to note text working

--- client/js/note.js
+++ client/js/note.js
@@ -1,15 +1,15 @@
 Template.note.events({
   'change .title': function(event,context) {
     Notes.update(this._id, {$set:{name:event.target.value}});
-  },
-  'change #summernote': function(event, context) {
-    console.log('change');
   }
 });
 
-Template.note.rendered = function(){
-  console.log("test")
-  var editor = $('#summernote')
-  editor.summernote();
+Template.note.onRendered(function(){
+  var editor = $('#summernote');
+  editor.summernote({
+    onChange: function(content, $editable) {
+      Notes.update(Session.get('note'),{$set: {content: content}});
+    }
+  });
   editor.code(this.content)
-}
+});
